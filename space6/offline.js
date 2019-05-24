145fb0afab897f373ac1c798e401a1b651632143
Gabriel753159 <50372227+Gabriel753159@users.noreply.github.com>
2019-05-24 19:45:12 -0300

    Add files via upload

---
+++ space6/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1558735798,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/nave-sheet0.png",
+		"images/tiro-sheet0.png",
+		"images/botão-sheet0.png",
+		"images/botão-sheet1.png",
+		"images/setacima-sheet0.png",
+		"images/setacima-sheet1.png",
+		"images/setabaixo-sheet0.png",
+		"images/setabaixo-sheet1.png",
+		"images/setadireita-sheet0.png",
+		"images/setadireita-sheet1.png",
+		"images/setaesquerda-sheet0.png",
+		"images/setaesquerda-sheet1.png",
+		"images/inimigo-sheet0.png",
+		"images/tiroinimigo-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
